3fe23ea50126d0f3298ea1122420ec69c244c3c2
maverick-gao <maverick.gao.us@gmail.com>
2023-02-17 17:06:52 +0800

    update the progress bar module and change the files output.

--- Python/mysql_insert_sqlfile.py
+++ Python/mysql_insert_sqlfile.py
@@ -1,66 +1,69 @@
 # Author:   Maverick Gao
 # Version:  V1.0 2023.02.17
 #           V1.1 2023.02.17
+#           V1.3 2023.02.18
 
-import sys
 import pymysql
-from tqdm import tqdm
+import sys
 
+# 定义连接信息
+host = 'localhost'
+user = 'root'
+password = 'password'
+db = 'test'
+port = 3306
 
-# 获取文件行数
-def get_file_lines(file_path):
-    with open(file_path, 'r') as f:
-        return sum(1 for line in f)
+# 定义文件路径
+filename = 'sql_file.sql'
+success_file = 'success.txt'
+failure_file = 'failure.txt'
 
+# 获取 sql 文件的总行数
+with open(filename, 'r', encoding='utf-8') as f:
+    total_lines = sum(1 for _ in f)
 
-def main():
-    # 数据库连接信息
-    db_host = 'localhost'
-    db_port = 3306
-    db_user = 'root'
-    db_password = '123456'
-    db_name = 'test'
-
-    # SQL文件路径
-    sql_file_path = 'test.sql'
-
-    # 获取文件总行数
-    total_lines = get_file_lines(sql_file_path)
-
+# 打开 sql 文件
+with open(filename, 'r', encoding='utf-8') as f:
+    # 初始化进度条和成功/失败的计数器
+    progress_count = 0
+    success_count = 0
+    failure_count = 0
+    
     # 连接数据库
-    conn = pymysql.connect(
-        host=db_host,
-        port=db_port,
-        user=db_user,
-        password=db_password,
-        db=db_name,
-        charset='utf8mb4'
-    )
-
-    # 获取游标对象
+    conn = pymysql.connect(host=host, user=user, password=password, db=db, port=port, charset='utf8mb4')
     cursor = conn.cursor()
 
-    # 打开SQL文件并逐行执行SQL语句
-    with open(sql_file_path, 'r') as f:
-        # 使用tqdm显示进度条
-        for line in tqdm(f, total=total_lines):
-            sql = line.strip()
-            if sql and not sql.startswith('--'):
-                try:
-                    cursor.execute(sql)
-                except pymysql.Error as e:
-                    with open('error.log', 'a') as log:
-                        log.write(f"{sql}\n{str(e)}\n")
+    # 逐行读取 sql 文件
+    for line in f:
+        try:
+            # 执行 sql 语句
+            cursor.execute(line)
 
-        # 提交事务
-        conn.commit()
+            # 记录插入成功的语句到文件
+            with open(success_file, 'a', encoding='utf-8') as sf:
+                sf.write(line)
 
-    # 关闭游标和连接
+            # 计数器加 1
+            success_count += 1
+
+        except Exception as e:
+            # 记录插入失败的语句和错误信息到文件
+            with open(failure_file, 'a', encoding='utf-8') as ff:
+                ff.write(line)
+                ff.write(str(e) + '\n')
+
+            # 计数器加 1
+            failure_count += 1
+
+        # 更新进度条
+        progress_count += 1
+        progress = int(progress_count / total_lines * 100)
+        sys.stdout.write(f"\rProgress: [{progress * '#'}{(100 - progress) * ' '}] {progress}%")
+        sys.stdout.flush()
+
+    # 关闭数据库连接
     cursor.close()
     conn.close()
 
-    print('SQL导入完成！')
-
-
-if __name__ == '__main__':
-    main()
+# 输出成功与失败的数量
+print(f"\nInserted successfully: {success_count}\nInsertion failed: {failure_count}")
